1694f4d2c41cf52c213fb6696aec67007a14a540
nikitenkovit <nelot@mail.ru>
2020-12-26 19:59:07 +0300

    finished task 3

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
-import {createSiteMenuTemplate} from "./view/site-menu.js";
-import {createFilterTemplate} from "./view/filter.js";
-import {createBordContainer} from "./view/bordContainer.js";
+import {createSiteMenuTemplate} from "./view/site-menu";
+import {createFilterTemplate} from "./view/filter";
+import {createBordContainer} from "./view/bordContainer";
 import {createBoardSortList} from "./view/bordSortList";
-import {createEditCard} from "./view/editCard.js";
-import {createCard} from "./view/card.js";
-import {createLoadMoreButton} from "./view/loadMoreButton.js";
+import {createTaskEditTemplate} from "./view/task-edit";
+import {createTaskTemplate} from "./view/task";
+import {createLoadMoreButton} from "./view/loadMoreButton";
+import {generateTask} from "./mock/task";
+import {generateFilter} from "./mock/filter.js";
 
-const TASK_COUNT = 3;
+const TASK_COUNT = 22;
+const TASK_COUNT_PER_STEP = 8;
+
+const tasks = new Array(TASK_COUNT).fill().map(generateTask);
+const filters = generateFilter(tasks);
 
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
@@ -16,17 +22,36 @@ const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
 render(siteHeaderElement, createSiteMenuTemplate());
-render(siteMainElement, createFilterTemplate());
+render(siteMainElement, createFilterTemplate(filters));
 render(siteMainElement, createBordContainer());
 
 const board = siteMainElement.querySelector(`.board`);
 const taskContainer = board.querySelector(`.board__tasks`);
 
 render(board, createBoardSortList(), `afterbegin`);
-render(taskContainer, createEditCard());
+render(taskContainer, createTaskEditTemplate(tasks[0]));
 
-for (let i = 0; i < TASK_COUNT; i++) {
-  render(taskContainer, createCard());
+for (let i = 1; i < Math.min(tasks.length, TASK_COUNT_PER_STEP); i++) {
+  render(taskContainer, createTaskTemplate(tasks[i]));
 }
 
-render(board, createLoadMoreButton());
+if (tasks.length > TASK_COUNT_PER_STEP) {
+  let renderedTaskCount = TASK_COUNT_PER_STEP;
+
+  render(board, createLoadMoreButton());
+
+  const loadMoreButton = board.querySelector(`.load-more`);
+
+  loadMoreButton.addEventListener(`click`, (evt) => {
+    evt.preventDefault();
+    tasks
+      .slice(renderedTaskCount, renderedTaskCount + TASK_COUNT_PER_STEP)
+      .forEach((task) => render(taskContainer, createTaskTemplate(task)));
+
+    renderedTaskCount += TASK_COUNT_PER_STEP;
+
+    if (renderedTaskCount >= tasks.length) {
+      loadMoreButton.remove();
+    }
+  });
+}
---
+++ src/mock/task.js
@@ -0,0 +1,70 @@
+import dayjs from "dayjs";
+import {COLORS} from "../const.js";
+import {getRandomInteger} from "../utils.js";
+
+const generateDescription = () => {
+  const descriptions = [
+    `Изучить теорию`,
+    `Сделать домашку`,
+    `Пройти интенсив на соточку`
+  ];
+
+  const randomIndex = getRandomInteger(0, descriptions.length - 1);
+
+  return descriptions[randomIndex];
+};
+
+const generateDate = () => {
+  const isDate = Boolean(getRandomInteger(0, 1));
+
+  if (!isDate) {
+    return null;
+  }
+
+  const maxDaysGap = 7;
+  const daysGap = getRandomInteger(-maxDaysGap, maxDaysGap);
+
+  return dayjs().add(daysGap, `day`).toDate();
+};
+
+const generateRepeating = () => {
+  return {
+    mo: false,
+    tu: false,
+    we: Boolean(getRandomInteger(0, 1)),
+    th: false,
+    fr: Boolean(getRandomInteger(0, 1)),
+    sa: false,
+    su: false
+  };
+};
+
+const getRandomColor = () => {
+  const randomIndex = getRandomInteger(0, COLORS.length - 1);
+
+  return COLORS[randomIndex];
+};
+
+export const generateTask = () => {
+  const dueDate = generateDate();
+  const repeating = dueDate === null
+    ? generateRepeating()
+    : {
+      mo: false,
+      tu: false,
+      we: false,
+      th: false,
+      fr: false,
+      sa: false,
+      su: false
+    };
+
+  return {
+    description: generateDescription(),
+    dueDate,
+    repeating,
+    color: getRandomColor(),
+    isArchive: Boolean(getRandomInteger(0, 1)),
+    isFavorite: Boolean(getRandomInteger(0, 1))
+  };
+};
